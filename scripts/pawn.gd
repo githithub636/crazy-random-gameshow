12ca141e1821c39634842fa0fd8c56dc4e0a63b3
Kno._.Clu <game.boy3030@gmail.com>
2026-07-10 18:43:01 -0400

    set up a quadrant locater for my the pawn - move()
    The Pawn can now locate the player and act accordingly. It works by getting the angle between the pawn and the player and checks if its > or < than a certain value to determine its quadrant .

--- scripts/pawn.gd
+++ scripts/pawn.gd
@@ -11,18 +11,38 @@ var found_player = false
 
 var health = 3  
 
+var player_angle = 0
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if found_player == true:
-		if player.pos.x < position.x:
-			print("owow") 
+	player_angle = rad_to_deg((player.position - position).angle())
+	print(player_angle)
+	move()
+#player.pos.x < position.x:
+			
+func move():
+	#QUADRANT 1
+	if (player_angle <= 0 and player_angle >= -90):
+		print(1)
 		
+	#QUADRANT 2
+	elif (player_angle <= -90 and player_angle >= -180):
+		print(2)
+		
+	#QUADRANT 3
+	elif (player_angle >= 90 and player_angle <= 180):
+		print(3)
+		
+	#QUADRANT 4
+	elif (player_angle >= 0 and player_angle <= 90):
+		print(4)
 	
 func _on_search_box_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and found_player == false:
 		found_player = true
 		print(found_player)
-		
+	
 func take_damage(amount):
 	health -= amount
 	print("hello")

1a61c2101eba55aefe733da7add7a72a5daf4971
Kno._.Clu <game.boy3030@gmail.com>
2026-07-10 21:26:23 -0400

    trying to set up pawn dash

--- scripts/pawn.gd
+++ scripts/pawn.gd
@@ -13,19 +13,26 @@ var health = 3
 
 var player_angle = 0
 
+var can_move = true
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	player_angle = rad_to_deg((player.position - position).angle())
 	print(player_angle)
 	move()
+	move_and_slide()
 #player.pos.x < position.x:
 			
 func move():
 	#QUADRANT 1
 	if (player_angle <= 0 and player_angle >= -90):
 		print(1)
-		
+		if can_move == true:
+			velocity.x += 100
+			
+			can_move = false
+			
 	#QUADRANT 2
 	elif (player_angle <= -90 and player_angle >= -180):
 		print(2)
@@ -48,5 +55,4 @@ func take_damage(amount):
 	print("hello")
 	if health <= 0:
 		queue_free()
-		
 		

1aee7ec33c129554789f77f1c82962f07b296ff7
Kno._.Clu <game.boy3030@gmail.com>
2026-07-11 21:17:20 -0400

    The Pawn Can Dash 🥳
    Please add comments that explain how this works ... I'm begging you

--- scripts/pawn.gd
+++ scripts/pawn.gd
@@ -19,7 +19,14 @@ var can_move = true
 
 var attack_count = 0
 
+var attacking = false
+
+var attack_diretion 
+
+var direction = 0
+
 func _physics_process(delta: float) -> void:
+	direction = (player.global_position - global_position).normalized()
 	velocity = velocity.move_toward(Vector2.ZERO,5.0)
 	move()
 	move_and_slide()
@@ -27,7 +34,8 @@ func _physics_process(delta: float) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	player_angle = rad_to_deg((player.position - position).angle())
-	print(player_angle)
+	
+	attack_count = clamp(attack_count,0,3)
 	
 #player.pos.x < position.x:
 			
@@ -35,60 +43,84 @@ func move():
 
 	if found_player == true:		
 		#QUADRANT 1
-		if (player_angle <= 0 and player_angle >= -90):
+		if (player_angle <= 0 and player_angle >= -90 and attack_count != 3):
 			if can_move == true:
 				move_timer.start()
 				can_move = false
 				if randf() < 0.5:
 					velocity.x += 250
+					attack_count += 1
+					print(attack_count)
 				else:
 					velocity.y -= 250
-				attack_count +=1
+					attack_count += 1
+					print(attack_count)
 				
 		#QUADRANT 2
-		elif (player_angle <= -90 and player_angle >= -180):
+		elif (player_angle <= -90 and player_angle >= -180 and attack_count != 3):
 			if can_move == true:
 				move_timer.start()
 				can_move = false
 				if randf() < 0.5:
 					velocity.x -= 250
+					attack_count += 1
+					print(attack_count)
 				else:
 					velocity.y -= 250
-				attack_count +=1
+					attack_count += 1
+					print(attack_count)
 				
 			
 		#QUADRANT 3
-		elif (player_angle >= 90 and player_angle <= 180):
+		elif (player_angle >= 90 and player_angle <= 180 and attack_count != 3):
 			if can_move == true:
 				move_timer.start()
 				can_move = false
 				if randf() < 0.5:
 					velocity.x -= 250
+					attack_count += 1
+					print(attack_count)
 				else:
 					velocity.y += 250
-				attack_count +=1
+					attack_count += 1
+					print(attack_count)
 				
 			
 		#QUADRANT 4
-		elif (player_angle >= 0 and player_angle <= 90):
+		elif (player_angle >= 0 and player_angle <= 90 and attack_count != 3):
 			if can_move == true:
 				move_timer.start()
 				can_move = false
 				if randf() < 0.5:
 					velocity.x += 250
+					attack_count += 1
+					print(attack_count)
 				else:
 					velocity.y += 250
-				attack_count +=1
+					attack_count += 1
+					print(attack_count)
 				
-	
+		
+		
+		if  (attack_count == 3 and can_move == true and attacking == false):
+			print("yolo")
+			attacking = true
+			velocity += direction * 400.0
+			
+		if (attack_count == 3 and attacking == true):
+			if velocity == Vector2.ZERO:
+				attack_count = 0
+				attacking = false
+			
+			
 func _on_search_box_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") and found_player == false:
 		found_player = true
-		print(found_player)
+		
 	
 func take_damage(amount):
 	health -= amount
-	print("hello")
+	
 	if health <= 0:
 		queue_free()
 		
